181c6a218a87fda1021beb9f9c306f4b1c79d50d
James Hoofard <james.hoofard.iii@gmail.com>
2021-09-26 14:29:44 -0500

    added model schema

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const PORT = (process.env.PORT || 9001);
+
+const app = express();
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static('public'))
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workouts_db', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+})
+
+app.use(require('./routes/index.js'))
+
+app.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`)
+})
